257aea4e2b210b8e4443168dc6f79e7a4e61ea9d
karislewis <131800825+karislewis@users.noreply.github.com>
2024-10-28 13:20:41 -0400

    Update wall_follower.py

--- wall_follower.py
+++ wall_follower.py
@@ -15,8 +15,20 @@ def find_min_dist(ranges, thetas):
     Returns:
         tuple: The length and angle of the shortest ray in the Lidar scan.
     """
-    robot.read_lidar_scan(ranges)
+
     min_dist, min_angle = None, None
+    valid_ranges = np.array(ranges)
+    valid_thetas = np.array(thetas)
+
+    valid_index= (valid_ranges > 0).nonzero()[0]
+    if len(valid_index)==0:
+        return None, None
+
+    min_index = valid_ranges[valid_index].argmin()
+    min_dist = valid_ranges[valid_index][min_index]
+    min_angle = valid_thetas[valid_index][min_index]
+
+    
 
     # TODO: Find the length and angle of the shortest distance in the ray.
 
@@ -43,15 +55,20 @@ setpoint = 0  # TODO: Pick your setpoint.
 # TODO: Declare any other variables you might need here.
 
 try:
-    while True:
-        # Read the latest lidar scan.
-        ranges, thetas = robot.read_lidar()
+    # Read the latest lidar scan.
+    ranges, thetas = robot.read_lidar()
+    min_dist, min_angle=find_min_dist(ranges,thetas)
+    
+    for i in range(2):
+        print(min_dist,min_angle)
+    
 
         # TODO: (P1.2) Write code to follow the nearest wall here.
         # Hint: You should use the functions cross_product and find_min_dist.
 
         # Optionally, sleep for a bit before reading a new scan.
         time.sleep(0.1)
-except:
+except KeyboardInterrupt:
+   print("Control+C pressed.")
     # Catch any exception, including the user quitting, and stop the robot.
     robot.stop()
